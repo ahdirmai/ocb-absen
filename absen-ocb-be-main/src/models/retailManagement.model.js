8fa8132a38e05d8249d20c5afe209982cd9e4be3
ahdirmai <ridhofahmij225@gmail.com>
2026-07-24 17:04:53 +0800

    fix: tambah uses_jadwal_harian ke retail create/update + parameterize SQL

--- absen-ocb-be-main/src/models/retailManagement.model.js
+++ absen-ocb-be-main/src/models/retailManagement.model.js
@@ -13,19 +13,19 @@ const getRetail = async(retailId) =>{
 }
 
 const updateRetail = async(body, retailId) =>{
-    const SQLQuery = `UPDATE retail 
-                        SET name ='${body.name}',latitude = '${body.latitude}',longitude = '${body.longitude}',radius = '${body.radius}', is_active = '${body.is_active}', updated_at ='${body.updated_at}',updated_by = '${body.updated_by}' 
-                        WHERE retail_id =${retailId}`;
-
-    return dbpool.execute(SQLQuery);
+    const SQLQuery = `UPDATE retail
+                        SET name = ?, latitude = ?, longitude = ?, radius = ?, is_active = ?, uses_jadwal_harian = ?, updated_at = ?, updated_by = ?
+                        WHERE retail_id = ?`;
+    const values = [body.name, body.latitude, body.longitude, body.radius, body.is_active, body.uses_jadwal_harian || 0, body.updated_at, body.updated_by, retailId];
+    return dbpool.execute(SQLQuery, values);
 }
 
 
 const createNewRetail = async(body)=>{
 
     const [result] = await dbpool.query(
-        'INSERT INTO retail (name, latitude, longitude, radius, is_active, created_at, created_by )VALUES (?,?,?,?,?,?,?)',
-        [body.name, body.latitude, body.longitude, body.radius, body.is_active, body.created_at, body.created_by]
+        'INSERT INTO retail (name, latitude, longitude, radius, is_active, uses_jadwal_harian, created_at, created_by) VALUES (?,?,?,?,?,?,?,?)',
+        [body.name, body.latitude, body.longitude, body.radius, body.is_active, body.uses_jadwal_harian || 0, body.created_at, body.created_by]
     );
     return result;
 
